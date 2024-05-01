f4b84576878f30f885445f0cc537da9656ff1965
Krishan <117340271+shoonya0@users.noreply.github.com>
2024-05-02 02:07:23 +0530

    -Added enemy to the games with basic movements.

--- src/gameStates/Playing.java
+++ src/gameStates/Playing.java
@@ -7,6 +7,7 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.util.Random;
 
+import entities.EnemyManager;
 import entities.Player;
 import levels.LevelManager;
 import main.Game;
@@ -19,6 +20,7 @@ public class Playing extends State implements Statemethods{
 
 	private Player player;
 	private LevelManager levelManager;
+	private EnemyManager enemyManager;
 	private PauseOverlay pauseOverlay;
 	private boolean paused = false;
 	
@@ -56,6 +58,7 @@ public class Playing extends State implements Statemethods{
 	
 	private void initClasses() {
 		levelManager = new LevelManager(game);
+		enemyManager = new EnemyManager(this);
 		player = new Player(200, 200 ,(int)(64*Game.SCALE) ,(int)(40*Game.SCALE));
 		player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
 		pauseOverlay = new PauseOverlay(this);
@@ -66,6 +69,7 @@ public class Playing extends State implements Statemethods{
 		if(!paused) {
 			levelManager.update();
 			player.update();	
+			enemyManager.update();
 			checkCloseToBorder();
 		}else {
 			pauseOverlay.update();			
@@ -103,7 +107,7 @@ public class Playing extends State implements Statemethods{
 		
 		levelManager.draw(g ,xLevelOffset);
 		player.render(g ,xLevelOffset);
-		
+		enemyManager.draw(g ,xLevelOffset);
 		if(paused) {
 			g.setColor(new Color(0 ,0 ,0 ,200));
 			g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT);
--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -5,17 +5,21 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
+import entities.Crabby;
 import main.Game;
+import static utilz.Constants.EnemyConstants.*;
+
 
 public class LoadSave {
 	
 	public static final String PLAYER_ALTAS = "player_sprites.png";
 	public static final String LEVEL_ALTAS = "outside_sprites.png";
 //	public static final String LEVEL_ONE_DATA = "level_one_data.png";
-	public static final String LEVEL_ONE_DATA = "level_one_data_long_.png";
+	public static final String LEVEL_ONE_DATA = "level_one_data_long.png";
 	public static final String MENU_BUTTONS = "button_atlas.png";
 	public static final String MENU_BACKGROUND = "menu_background.png";
 	public static final String PAUSE_BACKGROUND = "pause_menu.png";
@@ -27,6 +31,7 @@ public class LoadSave {
 	public static final String PLAYING_BACKGROUND_IMG = "playing_bg_img.png";
 	public static final String BIG_CLOUDS = "big_clouds.png";
 	public static final String SMALL_CLOUDS = "small_clouds.png";
+	public static final String CRABBY_SPRITE = "crabby_sprite.png";
 	
 //	here we will not have constructor because we use static method so we do have to create an obj of this class to access any method
 	public static BufferedImage GetSpriteAtlas(String fileName) {
@@ -50,6 +55,24 @@ public class LoadSave {
 		}
 //		throw is use for modified exception like if you want to throw an exception when the input is greater than 10
 		return img;
+	}
+	
+	public static ArrayList<Crabby> GetCrabs(){
+		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+		ArrayList<Crabby> list = new ArrayList<>();
+		
+		for(int j = 0; j < img.getHeight(); j++) {
+			for(int i = 0; i < img.getWidth() ;i++) {
+//			i ,j -> because of x ,y	{width ,height}
+//				to get the image of the particular pixel
+				Color color = new Color(img.getRGB(i, j));
+				int value = color.getGreen();
+//				in case of we get the value more then 48 
+				if(value == CRABBY)
+					list.add(new Crabby(i * Game.TILES_SIZE, j * Game.TILES_SIZE));
+			}
+		}
+		return list;
 	}
 	
 //	return an 2d array and the size of this array will match the size of our game window in terms of tile per width and height
